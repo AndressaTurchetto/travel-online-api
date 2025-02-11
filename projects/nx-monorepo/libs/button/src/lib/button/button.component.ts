bea737d523216068e8bea60858bce11ea7ddf096
Consórcio ASSES - COMPUTADORES <computadores@consorcioasses.onmicrosoft.com>
2025-02-11 15:49:11 -0300

    Criando a biblioteca com Storybook

---
+++ projects/nx-monorepo/libs/button/src/lib/button/button.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Variant = 'primary' | 'secondary' | 'tertiary';
+type Theme = 'blue' | 'violet';
+
+@Component({
+  selector: 'ab-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  /**
+   * Texto do botão.
+   */
+  @Input() text = '';
+  @Input() variant: Variant = 'primary';
+  @Input() disabled = false;
+  /**
+   * Tema do botão.
+   */
+  @Input() theme: Theme = 'blue';
+
+  getClasses(): string {
+    return `${this.variant} ${this.theme}`;
+  }
+}
